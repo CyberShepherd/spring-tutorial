e62b0168dc436aa435ce71f83f1e41279a295647
CyberShephard <g.modzelewski@protonmail.com>
2021-02-22 18:11:30 +0100

    Student mapping added

--- src/main/java/com/cybershephard/springtutorial/SpringTutorialApplication.java
+++ src/main/java/com/cybershephard/springtutorial/SpringTutorialApplication.java
@@ -1,21 +1,21 @@
 package com.cybershephard.springtutorial;
 
+import com.cybershephard.springtutorial.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTutorialApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello world";
-	}
+
 
 }
