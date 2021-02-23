e0f13ff7cf5f20b50f60065ec0ef2bc1ef8259ca
CyberShephard <g.modzelewski@protonmail.com>
2021-02-23 13:13:05 +0100

    api, service and data access connected

--- src/main/java/com/cybershephard/springtutorial/student/Student.java
+++ src/main/java/com/cybershephard/springtutorial/student/Student.java
@@ -1,11 +1,24 @@
 package com.cybershephard.springtutorial.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Simple pojo class for student data represantation
  */
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
--- src/main/java/com/cybershephard/springtutorial/student/StudentService.java
+++ src/main/java/com/cybershephard/springtutorial/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.cybershephard.springtutorial.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,7 +8,17 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+//    TODO: remove insert
     public List<Student> getStudents(){
-        return List.of(new Student(1L, "Gabon", "gabriel.m@op.pl", LocalDate.of(2000, 12, 3), 21));
+        Student student = new Student(1L,"gabriel", "gabriel.m@op.pl", LocalDate.of(2000,10,1), 21);
+        studentRepository.save(student);
+        return studentRepository.findAll();
     }
 }
