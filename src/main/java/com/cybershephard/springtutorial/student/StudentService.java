fe26d7847580998dfb4b80f566c85a02fba527e7
CyberShephard <g.modzelewski@protonmail.com>
2021-02-23 14:17:23 +0100

    POST GET functional

--- src/main/java/com/cybershephard/springtutorial/student/StudentService.java
+++ src/main/java/com/cybershephard/springtutorial/student/StudentService.java
@@ -15,12 +15,11 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-//    TODO: remove insert
     public List<Student> getStudents(){
-        Student student1 = new Student("gabriel", "gabriel.m@op.pl", LocalDate.of(2000,10,1), 21);
-        Student student2 = new Student("flair", "flair.mail@op.pl", LocalDate.of(1996,4,12), 25);
-        List<Student> students = List.of(student1, student2);
-        studentRepository.saveAll(students);
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+        studentRepository.save(student);
     }
 }
